47b7d8351dfacafb76fe5fb05b397f2e0a495b9e
Florian Reifschneider <florian@rocketloop.de>
2018-10-25 11:35:06 +0200

    Implement awesome-element component

--- awesome-element/src/app/app.component.ts
+++ awesome-element/src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'awesome-element',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.Native
 })
-export class AppComponent {
-  title = 'awesome-element';
+export class AppComponent implements OnChanges {
+  
+  /**
+   * The name of the thing or person that is awesome
+   */
+  @Input()
+  name: string;
+
+  /**
+   * Event fired when the awesome button is clicked
+   */
+  @Output()
+  awesomeClick: EventEmitter<void> = new EventEmitter();
+
+  /**
+   * The number of times the awesome button was clicked
+   */
+  clickCount = 0;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['name']) {
+      this.clickCount = 0;
+    }
+  }
+
+  /**
+   * Called when the awesome button is clicked
+   * @param e The event
+   */
+  onAwesomeButtonClicked(e: Event) {
+    this.clickCount++;
+    this.awesomeClick.next();
+  }
+
 }
